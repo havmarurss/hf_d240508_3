a8eb004203ca7933944ffe42ef2d94a102cce510
Marton Havasreti <marton.havasreti@urss.hu>
2024-05-09 13:06:58 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'hf_d240508_3';
+}
